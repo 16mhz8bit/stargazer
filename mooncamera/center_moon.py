b36e4516523918ffb82ea86b81f2c5e8bdba5c02
Leonardo Matteo Bolognese <leonardo.bolognese@epfl.ch>
2025-07-06 21:16:08 +0200

    Implemented filter for smaller objects

--- mooncamera/center_moon.py
+++ mooncamera/center_moon.py
@@ -1,18 +1,24 @@
 import cv2
+import numpy as np
 from helpers.centeroids import compute_centroids
 
-img = cv2.imread("imgs/7.jpeg")
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+for i in range(5, 15):
+    img = cv2.imread(f"imgs/{i}.jpeg")
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-_, thresh = cv2.threshold(img_gray, 180, 255, 0)
-cX, cY = compute_centroids(thresh)
+    _, thresh = cv2.threshold(img_gray, 200, 255, 0)
 
-cv2.circle(img, (cX, cY), 5, (255, 255, 255), -1)
+    kernel = np.ones((10, 10), np.uint8)
+    cleaned = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
 
-# text for the centroid
-cv2.putText(img, "centroid", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    cX, cY = compute_centroids(cleaned)
 
-cv2.imshow("Centroid", img)
-cv2.imshow("Thresholded Image", thresh)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.circle(img, (cX, cY), 5, (0, 255, 0), -1)
+
+    # text for the centroid
+    cv2.putText(img, "centroid", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    cv2.imshow("Centroid", img)
+    cv2.imshow("Thresholded Image", cleaned)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
